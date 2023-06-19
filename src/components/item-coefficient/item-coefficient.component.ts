168f66f6daffb5bf196b65694be4a00925cca20c
akrambenali <akram1304@gmail.com>
2023-06-19 21:42:23 +0200

    update  pages

--- src/components/item-coefficient/item-coefficient.component.ts
+++ src/components/item-coefficient/item-coefficient.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-item-coefficient',
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-coefficient.component.css']
 })
 export class ItemCoefficientComponent implements OnInit {
-
+  @Input()
+  title!: string;
+  @Input()
+  icon!: string;
   constructor() { }
 
   ngOnInit(): void {
--- src/features/client-coefficients-page/client-coefficients-page.component.ts
+++ src/features/client-coefficients-page/client-coefficients-page.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientCoefficientsPageComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
