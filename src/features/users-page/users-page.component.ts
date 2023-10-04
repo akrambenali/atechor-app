df2ad2ff0ed3050a0e8473b4a9031923558be7cd
akrambenali <akram1304@gmail.com>
2023-10-04 18:36:00 +0200

    upda favicon and  mobile screens

--- src/features/users-page/users-page.component.ts
+++ src/features/users-page/users-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CompanySize, Solutions } from 'src/business/model/solution.model';
+import { ResponsiveService } from '../services/responsive/responsive.service';
 
 @Component({
   selector: 'app-users-page',
@@ -11,7 +12,7 @@ export class UsersPageComponent implements OnInit {
   solutionModel = {} as Solutions;
   companySize = {} as CompanySize;
   isEmpty: boolean = true;
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, public responsive: ResponsiveService) {}
 
   ngOnInit(): void {
     this.solutionModel = history.state;
