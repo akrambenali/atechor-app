9d829aaba967c8a75cc9bf06dfaf4bdfb5e50365
akrambenali <akram1304@gmail.com>
2023-10-15 11:26:27 +0200

    🐛  fixe multi select deployment  && user page

--- src/features/client-deployment-page/client-deployment-page.component.ts
+++ src/features/client-deployment-page/client-deployment-page.component.ts
@@ -1,35 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { InternationnalBusiness, Solutions } from 'src/business/model/solution.model';
+import {
+  InternationnalBusiness,
+  Solutions,
+} from 'src/business/model/solution.model';
 import { ResponsiveService } from '../services/responsive/responsive.service';
 
 @Component({
   selector: 'app-client-deployment-page',
   templateUrl: './client-deployment-page.component.html',
-  styleUrls: ['./client-deployment-page.component.css']
+  styleUrls: ['./client-deployment-page.component.css'],
 })
 export class ClientDeploymentPageComponent implements OnInit {
-
   solutionModel = {} as Solutions;
   internationnalBusiness = {} as InternationnalBusiness;
   isEmpty: boolean = true;
-  
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute, public responsive: ResponsiveService) {
-    
-  }
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    public responsive: ResponsiveService
+  ) {}
 
   ngOnInit(): void {
-
-    this.solutionModel = history. state;
-     
+    this.solutionModel = history.state;
   }
 
   pushClientinternationnalBusiness(newItem: any) {
     this.internationnalBusiness = newItem;
+    this.checkDisabledItem(this.internationnalBusiness);
     this.solutionModel.internationnalBusiness = this.internationnalBusiness;
-    this.isEmpty =  false;
-    
+    this.isEmpty = false;
   }
 
+  checkDisabledItem(item: InternationnalBusiness) {
+    const N = document.getElementById('N');
+    const I = document.getElementById('I');
+
+    if (item.international === true) {
+      if (N != null) {
+        N.classList.add('disabled');
+      }
+    } else if (item.national === true) {
+      if (I != null) {
+        I.classList.add('disabled');
+      }
+    }
+  }
+
+  getStatus(status: boolean) {
+    const N = document.getElementById('N');
+    const I = document.getElementById('I');
+    if (!status) {
+      if (N != null && I != null) {
+        N.classList.remove('disabled');
+        I.classList.remove('disabled');
+      }
+    }
+  }
 }
